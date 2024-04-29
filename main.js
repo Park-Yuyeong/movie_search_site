97b2e91eeb3319a1691eeb1dd9a900bd8e000f98
Park-Yuyeong <dbduda1@kookmin.ac.kr>
2024-04-29 12:22:49 +0900

    주석 처리

--- main.js
+++ main.js
@@ -14,23 +14,29 @@ fetch('https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1', opti
     const res = response['results'];
 
     res.forEach(element => {
-
       let temp_html = `
-      <div class="card" onclick="alert('영화 id: ${element['id']}')">
-        <img src="https://image.tmdb.org/t/p/w500${element['poster_path']}" alt="이미지 오류">
-        <h3>${element['title']}</h3>
-        <p>${element['overview']}</p>
-        <p>Rating: ${element['vote_average']}</p>
-      </div>`;
+        <div class="card" id="${element['id']}" onclick={alert_id(id)}>
+          <img src="https://image.tmdb.org/t/p/w500${element['poster_path']}" alt="이미지 오류">
+          <h3>${element['title']}</h3>
+          <p>${element['overview']}</p>
+          <p>Rating: ${element['vote_average']}</p>
+        </div>`;
 
       card_section.innerHTML += temp_html;
     });
   })
   .catch(err => console.error(err));
 
+// 카드 클릭 시 카드 이미지 alert
+// 매개변수 id를 필요로 하는 화살표 함수
+const alert_id = (id) => {
+  alert(`영화 id: ${id}`);
+}
+
 const search_btn = document.getElementById('search_btn');
 const card = document.getElementsByClassName('card'); 
 
+// 검색 기능
 const find_movie = () => {
   const search_input = document.getElementById('search_input').value.toLowerCase();
 
